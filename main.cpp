679465387340dc195d0c968900ef3140c664742e
Rukhsora Toirova <99185813+migilarore@users.noreply.github.com>
2023-04-08 13:50:03 +0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//task 1
+//    int a, b;
+//    cout << "Enter the first integer:" << endl;
+//    cin >> a;
+//    cout << "Enter the second integer:" << endl;
+//    cin >> b;
+//    int c = a + b;
+//    int d = a * b;
+//    cout << "Sum:" << c << endl << "Multiplication: " << d << endl;
+
+
+//task 2
+//    int r;
+//    double pi = 3.14;
+//    cout << "Enter the radius of circle:" << endl;
+//    cin >> r;
+//    double d = 2 * r;
+//    double l = 2 * pi * r;
+//    double s = pi * r * r;
+//    cout << "Diameter:" << d << "\nCircumference: " << l << "\nArea:" << s << endl;
+
+
+//task 3
+//        double Celsius;
+//        cout << "Enter the temperature in Celsius: ";
+//        cin >> Celsius;
+//        double Farenheit = (Celsius * 1.8) + 32;
+//        cout << "The temperature in Farenheit: " << Farenheit;
+
+
+//task 4
+//    int N;
+//    cout << "Enter the number of students: " << endl;
+//    cin >> N;
+//    int l = 24 - (N % 24);
+//    cout << "In computer programming lab there are " << l << " free computers out of 24 computers.";
+
+
+//task 5
+//    int a, b;
+//    cout << "Enter the first integer:" << endl;
+//    cin >> a;
+//    cout << "Enter the second integer:" << endl;
+//    cin >> b;
+//    if (a >= b) {
+//        cout << "The answer is 1";
+//    }
+//    else {
+//        cout << "The answer is 0";
+//    }
+    return 0;
+}
